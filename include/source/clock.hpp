beb90736446309aaa678a3df04fddd232981e5bb
cameron <cameron.murtagh00@gmail.com>
2024-01-09 14:01:28 +1100

    format

--- include/source/clock.hpp
+++ include/source/clock.hpp
@@ -6,15 +6,16 @@
 
 namespace mcw::source {
 
-class clock {
-public:
-    clock(int interval, const std::string& format);
-    sigc::signal<void(const std::string&)> tick;
-private:
-    bool timeout_handler();
-    std::string format;
-};
+    class clock {
+    public:
+        clock(int interval, const std::string& format);
+        sigc::signal<void(const std::string&)> tick;
 
-}
+    private:
+        bool timeout_handler();
+        std::string format;
+    };
 
-#endif // CLOCK_HPP
+}  // namespace mcw::source
+
+#endif  // CLOCK_HPP
